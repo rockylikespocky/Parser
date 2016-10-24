657f5b798bfd016b410cfd3e3a36b2773acf71c3
Nhat Nguyen <rockylikespocky@users.noreply.github.com>
2016-10-23 21:36:37 -0700

    Updated Lexer.cpp & started on Parser.cpp

---
+++ Lexer.cpp
@@ -0,0 +1,469 @@
+//
+//  Lexer.cpp
+//  Parser
+//
+//  Created by Nhat Nguyen on 10/10/16.
+//  Created by Adam Adame on 10/10/16.
+//  Copyright © 2016 Nhat Nguyen & Adam Adame. All rights reserved.
+//
+
+#include "Lexer.h"
+
+Lexer::Lexer() {
+    // code input using stdin getline
+    
+    cout << "Enter input: " << endl;
+    ofstream outFile;
+    outFile.open("output.txt");
+    
+    while (getline(cin, line)) {
+        if (line == "QUIT" || line == "q" || line == "Q") {
+            outFile.close();
+            break;
+        }
+        
+        outFile << line << "\n";
+        
+        if (cin.bad()) {
+            // IO error
+            break;
+        }
+        
+    }
+    
+    Lexer m("output.txt");
+}
+
+Lexer::Lexer(string file_name) {
+    inFile.open(file_name);
+    
+    if (inFile.is_open()) {
+        cout << "Success: File is opened." << endl;
+        cout << "~~~~~Starting Lexer~~~~~" << endl;
+    }
+    if (!inFile.is_open()) {
+        cout << "Error: File won't open." << endl;
+    }
+    
+    // loop through file to get input
+    while(!atEndOfFile()) {
+        if (inFile.eof()) break; // Ugly but stops the output repeat of last line
+        getline(inFile, line);
+        
+        // Iterate through each char in string line
+        for (std::string::size_type i = 0; i < line.size(); ++i) {
+            char c = line[i];
+            char c2 = line[i + 1];
+            
+            if (isSpace(c)) {
+                continue;
+            }
+            
+            // Testing for string token
+            if (c == '"') {
+                ++i;
+                c = line[i];
+                c2 = line[i + 1];
+                while (!(c == '"')) {
+                    c = line[i];
+                    c2 = line[i + 1];
+                    if (c == '"') continue;
+                    tempstr += c;
+                    ++i;
+                }
+                token = tempstr;
+                type = "string";
+                setTokenID();
+                saveInfo();
+                printOutput();
+                resetFields();
+            }
+            
+            // Testing for Unpaired delimiters
+            if (isUnpairedDelimiters(c)) {
+                token = c;
+                setTokenID();
+                saveInfo();
+                printOutput();
+                resetFields();
+                continue;
+            }
+            // Testing for Multi-char operators
+            if (isMultiOperator(c, c2)) {
+                token = c;
+                token += c2;
+                type = "multioperator";
+                setTokenID();
+                saveInfo();
+                printOutput();
+                ++i;
+                resetFields();
+            }
+            // Testing for Punctuations
+            else if (isPunctuation(c)) {
+                if (isComment(c, c2)) {
+                    break;
+                }
+                else {
+                    if (c2 == ' ') {
+                        token = c;
+                        type = "punctuation";
+                        setTokenID();
+                        saveInfo();
+                        printOutput();
+                    }
+                    resetFields();
+                }
+            }
+            // Testing for Delimeters
+            else if (isDelimeter(c)) {
+                token = c;
+                type = "delimiter";
+                setTokenID();
+                saveInfo();
+                printOutput();
+                resetFields();
+            }
+            // Testing for Integer
+            else if (isdigit(c)) {
+                tempstr += c;
+                type = "integer";
+                while (isdigit(c2)) {
+                    ++i;
+                    c = line[i];
+                    c2 = line[i + 1];
+                    tempstr += c;
+                }
+                // Testing for Float
+                if (c2 == '.') {
+                    ++i;
+                    c = line[i];
+                    c2 = line[i + 1];
+                    tempstr += c;
+                    type = "float";
+                    while (isdigit(c2)) {
+                        ++i;
+                        c = line[i];
+                        c2 = line[i + 1];
+                        tempstr += c;
+                    }
+                }
+                token = tempstr;
+                setTokenID();
+                saveInfo();
+                printOutput();
+                resetFields();
+            }
+            // currently a keyword or identifier
+            else if (isalpha(c)) {
+                tempstr += c;
+                while (isalpha(c2) || isdigit(c2)) {
+                    i++;
+                    c = line[i];
+                    c2 = line[i + 1];
+                    tempstr += c;
+                }
+                if (isKeyword(tempstr)) {
+                    token = tempstr;
+                    setTokenID();
+                    saveInfo();
+                    printOutput();
+                    resetFields();
+                }
+                else {
+                    type = "identifier";
+                    token = tempstr;
+                    setTokenID();
+                    saveInfo();
+                    printOutput();
+                    resetFields();
+                }
+            }
+        }
+        lineCount += 1;
+    }
+    endingProcess();
+}
+
+void Lexer::printOutput() {
+    cout << "(Tok: id= "
+    << idNumber
+    << " line= "
+    << lineCount
+    << " str= "
+    << '"'
+    << token
+    << '"';
+    
+    if (type == "integer") {
+        cout << " int= " << token;
+    }
+    if (type == "float") {
+        cout << " float= " << token;
+    }
+    
+    cout << ")"
+    << endl;
+}
+
+void Lexer::saveInfo() {
+    Token myToken;
+    myToken.lineNumber = lineCount;
+    myToken.tokenID = idNumber;
+    myToken.tokenItem = token;
+    Queue.push(myToken);
+}
+
+void Lexer::setTokenID() {
+    // identifier ID
+    if (type == "identifier") {
+        idNumber = 2;
+        return;
+    }
+    // integer ID
+    if (type == "integer") {
+        idNumber = 3;
+    }
+    // float ID
+    if (type == "float") {
+        idNumber = 4;
+    }
+    // string ID
+    if (type == "string") {
+        idNumber = 5;
+    }
+    // unpaired delimiter IDs
+    if (token == ",") {
+        idNumber = 6;
+    }
+    if (token == ";") {
+        idNumber = 7;
+    }
+    // keyword IDs
+    if (token == "prog") {
+        idNumber = 10;
+    }
+    if (token == "main") {
+        idNumber = 11;
+    }
+    if (token == "fnc") {
+        idNumber = 12;
+    }
+    if (token == "class") {
+        idNumber = 13;
+    }
+    if (token == "float") {
+        idNumber = 15;
+    }
+    if (token == "int") {
+        idNumber = 16;
+    }
+    if (token == "string") {
+        idNumber = 17;
+    }
+    if (token == "if") {
+        idNumber = 18;
+    }
+    if (token == "elseif") {
+        idNumber = 19;
+    }
+    if (token == "else") {
+        idNumber = 20;
+    }
+    if (token == "while") {
+        idNumber = 21;
+    }
+    if (token == "input") {
+        idNumber = 22;
+    }
+    if (token == "print") {
+        idNumber = 23;
+    }
+    if (token == "new") {
+        idNumber = 24;
+    }
+    if (token == "return") {
+        idNumber = 25;
+    }
+    // delimiter IDs
+    if (token == "<") {
+        idNumber = 31;
+    }
+    if (token == ">") {
+        idNumber = 32;
+    }
+    if (token == "{") {
+        idNumber = 33;
+    }
+    if (token == "}") {
+        idNumber = 34;
+    }
+    if (token == "[") {
+        idNumber = 35;
+    }
+    if (token == "]") {
+        idNumber = 36;
+    }
+    if (token == "(") {
+        idNumber = 37;
+    }
+    if (token == ")") {
+        idNumber = 38;
+    }
+    // punctuation IDs
+    if (token == "*") {
+        idNumber = 41;
+    }
+    if (token == "^") {
+        idNumber = 42;
+    }
+    if (token == ":") {
+        idNumber = 43;
+    }
+    if (token == ".") {
+        idNumber = 44;
+    }
+    if (token == "=") {
+        idNumber = 45;
+    }
+    if (token == "-") {
+        idNumber = 46;
+    }
+    if (token == "+") {
+        idNumber = 47;
+    }
+    if (token == "/") {
+        idNumber = 48;
+    }
+    // multi-char operators
+    if (token == "->") {
+        idNumber = 51;
+    }
+    if (token == "==") {
+        idNumber = 52;
+    }
+    if (token == "!=") {
+        idNumber = 53;
+    }
+    if (token == "<=") {
+        idNumber = 54;
+    }
+    if (token == ">=") {
+        idNumber = 55;
+    }
+    if (token == "<<") {
+        idNumber = 56;
+    }
+    if (token == ">>") {
+        idNumber = 57;
+    }
+    // error token ID
+    if (type == "error") {
+        idNumber = 99;
+    }
+}
+
+void Lexer::resetFields() {
+    idNumber = 0;
+    token = "";
+    tempstr = "";
+    type = "";
+}
+
+bool Lexer::isComment(char c1, char c2) {
+    return ((c1 == '/') && (c2 == '/'));
+}
+
+bool Lexer::isSpace(char c) {
+    return (c == ' ');
+}
+
+bool Lexer::isUnpairedDelimiters(char c) {
+    return (c == ',' || c == ';');
+}
+
+bool Lexer::isPunctuation(char c) {
+    return (
+            c == '*'
+            || c == '^'
+            || c == ':'
+            || c == '.'
+            || c == '='
+            || c == '-'
+            || c == '+'
+            || c == '/'
+            );
+}
+
+bool Lexer::isDelimeter(char c) {
+    return (
+            c == '<'
+            || c == '>'
+            || c == '{'
+            || c == '}'
+            || c == '['
+            || c == ']'
+            || c == '('
+            || c == ')'
+            );
+}
+
+bool Lexer::isMultiOperator(char c1, char c2) {
+    return (
+            (c1 == '-' && c2 == '>')
+            || (c1 == '=' && c2 == '=')
+            || (c1 == '!' && c2 == '=')
+            || (c1 == '<' && c2 == '=')
+            || (c1 == '>' && c2 == '=')
+            || (c1 == '<' && c2 == '<')
+            || (c1 == '>' && c2 == '>')
+            );
+}
+
+bool Lexer::isKeyword(string word) {
+    return (
+            word == "prog"
+            || word == "main"
+            || word == "fnc"
+            || word == "class"
+            || word == "float"
+            || word == "int"
+            || word == "string"
+            || word == "if"
+            || word == "elseif"
+            || word == "else"
+            || word == "while"
+            || word == "input"
+            || word == "print");
+}
+
+bool Lexer::atEndOfFile() {
+    if (!inFile) {
+        closeFile();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Lexer::closeFile() {
+    inFile.close();
+}
+
+void Lexer::endingProcess() {
+    idNumber = 0;
+    lineCount -= 2;
+    printOutput();
+    
+    // Testing to make sure that Queue is properly pushing Tokens
+    cout << "Testing Queue: " << endl;
+    for (int i = 0; (Queue.size() != 0); i++) {
+        Token currentToken = Queue.front();
+        cout << currentToken.tokenID << currentToken.tokenItem << endl;
+        Queue.pop();
+    }
+}
+
+queue<Token> Lexer::getQueue() {
+    return Queue;
+}
---
+++ Parser.h
@@ -0,0 +1,144 @@
+//
+//  Parser.h
+//  Parser
+//
+//  Created by Nhat Nguyen on 10/10/16.
+//  Created by Adam Adame on 10/10/16.
+//  Copyright © 2016 Nhat Nguyen & Adam Adame. All rights reserved.
+//
+
+#ifndef Parser_h
+#define Parser_h
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+class Parser {
+    struct cell {
+        int theID;
+        string theLeftSide;
+        string theRightSide;
+        void setCell(int idNumber, string lhs, string rhs) {
+            theID = idNumber;
+            theLeftSide = lhs;
+            theRightSide = rhs;
+        }
+    };
+    
+    //This cell 0 will be used to throw the error for the blank spaces in the matrix.
+    cell myCell0;
+    myCell0.setCell(0, "", "");
+    
+    cell myCell1;
+    myCell1.setCell(1, "Pgm", "kwdprog brace1 Slist brace2");
+    
+    cell myCell2;
+    myCell2.setCell(2, "Slist", "Stmt semi Slist");
+    
+    cell myCell3;
+    myCell3.setCell(3, "Slist", "eps");
+    
+    cell myCell4;
+    myCell4.setCell(4, "Stmt", "S_Out");
+    
+    cell myCell5;
+    myCell5.setCell(5, "Stmt", "id equal Y1");
+    
+    cell myCell6;
+    myCell6.setCell(6, "S_Out", "kwdprint paren1 Elist paren2");
+    
+    cell myCell7;
+    myCell7.setCell(7, "Y1", "E");
+    
+    cell myCell8;
+    myCell8.setCell(8, "Y1", "kwdinput");
+    
+    cell myCell9;
+    myCell9.setCell(9, "Elist", "E Elist2");
+    
+    cell myCell10;
+    myCell10.setCell(10, "Elist", "eps");
+    
+    cell mycell1;
+    myCel11.setCell(11, "Elist2", "comma Elist");
+    
+    cell myCell12;
+    myCell12.setCell(12, "E", "T X1");
+    
+    cell myCell13;
+    myCell13.setCell(13, "X1", "Opadd T X1");
+    
+    cell myCell14;
+    myCel14.setCell(14, "X1", "eps");
+    
+    cell mycell15;
+    myCel15.setCell(15, "T", "F X2");
+    
+    cell myCell16;
+    myCel16.setCell(16, "X2", "Opmul F X2");
+    
+    cell myCell117;
+    myCell17.setCell(17, "X2", "eps");
+    
+    cell mycell18;
+    myCell18.setCell(18, "F", "Fatom");
+    
+    cell myCell19;
+    myCell19.setCell(19, "F", "paren1 E paren2");
+    
+    cell myCell20;
+    myCell20.setCell(20, "Fatom", "id");
+    
+    cell myCell21;
+    myCell21.setCell(21, "Fatom", "int");
+    
+    cell myCell22;
+    myCell22.setCell(22, "Fatom", "float");
+    
+    cell myCell23;
+    myCell23.setCell(23, "Fatom", "string");
+    
+    cell myCell24;
+    myCel24.setCell(24, "Opadd", "plus");
+    
+    cell myCell25;
+    myCell25.setCell(25, "Opadd", "minus");
+    
+    cell myCell26;
+    myCell26.setCell(26, "Opmul", "aster");
+    
+    cell myCell27;
+    myCell27.setCell(27, "Opmul", "slash");
+    
+    cell myCell28;
+    myCell28.setCell(28, "Opmul", "caret");
+    
+    cell theMatrix[15][19] =
+    {
+    //              0        1        2        3         4         5         6          7        8         9         10         11        12        13        14        15        16      17        18
+    //              prog     print    input    id        int       float     string     (        )         ,          -         +         *         /         ^         ;         {        }        $
+    /* 0  Pgm   */{ myCell1, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 1  Slist */{ myCell0, myCell2, myCell0, myCell2,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell3, myCell0 },
+    /* 2  Stmt  */{ myCell0, myCell4, myCell0, myCell5,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 3  S_Out */{ myCell0, myCell0, myCell6, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 4  Y1    */{ myCell0, myCell0, myCell8, myCell7,  myCell7,  myCell7,  myCell7,  myCell7,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 5  Elist */{ myCell0, myCell0, myCell0, myCell9,  myCell9,  myCell9,  myCell9,  myCell9,  myCell10, myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 6  Elist2*/{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell11, myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 7  E     */{ myCell0, myCell0, myCell0, myCell12, myCell12, myCell12, myCell12, myCell12, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 8  X1    */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell14, myCell14, myCell13,  myCell13, myCell0,  myCell0,  myCell0, myCell14, myCell0, myCell0, myCell0 },
+    /* 9  T     */{ myCell0, myCell0, myCell0, myCell15, myCell15, myCell15, myCell15, myCell15, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 10 X2    */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell17, myCell17, myCell17,  myCell17, myCell16, myCell16, myCell16, myCell17, myCell0, myCell0, myCell0 },
+    /* 11 F     */{ myCell0, myCell0, myCell0, myCell18, myCell18, myCell18, myCell18, myCell19, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 12 Fatom */{ myCell0, myCell0, myCell0, myCell20, myCell21, myCell22, myCell23, myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 13 Opadd */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell25, myCell25,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
+    /* 14 Opmul */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell26, myCell27, myCell28, myCell0, myCell0, myCell0, myCell0 },
+    };
+};
+
+#endif /* Parser_h */
