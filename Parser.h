943e40984dbda38d7a9e488bb9dd4429e6564471
aadame13 <alan.adame13@gmail.com>
2016-10-23 22:47:28 -0700

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -26,91 +26,91 @@ struct cell {
 		
     };
 
+cell myCell0 = { 0, "", "" };
 
+cell myCell1 = { 1, "Pgm", "kwdprog brace1 Slist brace2" };
+
+cell myCell2 = { 2, "Slist", "Stmt semi Slist" };
+
+cell myCell3 = { 3, "Slist", "eps" };
+
+cell myCell4{ 4, "Stmt", "S_Out" };
+
+cell myCell5 = { 5, "Stmt", "id equal Y1" };
+
+cell myCell6 = { 6, "S_Out", "kwdprint paren1 Elist paren2" };
+
+cell myCell7 = { 7, "Y1", "E" };
+
+cell myCell8 = { 8, "Y1", "kwdinput" };
+
+cell myCell9 = { 9, "Elist", "E Elist2" };
+
+cell myCell10 = { 10, "Elist", "eps" };
+
+cell myCell11 = { 11, "Elist2", "comma Elist" };
+
+cell myCell12 = { 12, "E", "T X1" };
+
+cell myCell13 = { 13, "X1", "Opadd T X1" };
+
+cell myCell14 = { 14, "X1", "eps" };
+
+cell myCell15 = { 15, "T", "F X2" };
+
+cell myCell16 = { 16, "X2", "Opmul F X2" };
+
+cell myCell17 = { 17, "X2", "eps" };
+
+cell myCell18 = { 18, "F", "Fatom" };
+
+cell myCell19 = { 19, "F", "paren1 E paren2" };
+
+cell myCell20 = { 20, "Fatom", "id" };
+
+cell myCell21 = { 21, "Fatom", "int" };
+
+cell myCell22 = { 22, "Fatom", "float" };
+
+cell myCell23 = { 23, "Fatom", "string" };
+
+cell myCell24 = { 24, "Opadd", "plus" };
+
+cell myCell25 = { 25, "Opadd", "minus" };
+
+cell myCell26 = { 26, "Opmul", "aster" };
+
+cell myCell27 = { 27, "Opmul", "slash" };
+
+cell myCell28 = { 28, "Opmul", "caret" };
+
+cell theMatrix[15][19] =
+{
+	//              0        1        2        3         4         5         6          7        8         9         10         11        12        13        14        15        16      17        18
+	//              prog     print    input    id        int       float     string     (        )         ,          -         +         *         /         ^         ;         {        }        $
+	/* 0  Pgm   */{ myCell1, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 1  Slist */{ myCell0, myCell2, myCell0, myCell2, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell3, myCell0 },
+	/* 2  Stmt  */{ myCell0, myCell4, myCell0, myCell5, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 3  S_Out */{ myCell0, myCell0, myCell6, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 4  Y1    */{ myCell0, myCell0, myCell8, myCell7, myCell7, myCell7, myCell7, myCell7, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 5  Elist */{ myCell0, myCell0, myCell0, myCell9, myCell9, myCell9, myCell9, myCell9, myCell10, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 6  Elist2*/{ myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell11, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 7  E     */{ myCell0, myCell0, myCell0, myCell12, myCell12, myCell12, myCell12, myCell12, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 8  X1    */{ myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell14, myCell14, myCell13, myCell13, myCell0, myCell0, myCell0, myCell14, myCell0, myCell0, myCell0 },
+	/* 9  T     */{ myCell0, myCell0, myCell0, myCell15, myCell15, myCell15, myCell15, myCell15, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 10 X2    */{ myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell17, myCell17, myCell17, myCell17, myCell16, myCell16, myCell16, myCell17, myCell0, myCell0, myCell0 },
+	/* 11 F     */{ myCell0, myCell0, myCell0, myCell18, myCell18, myCell18, myCell18, myCell19, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 12 Fatom */{ myCell0, myCell0, myCell0, myCell20, myCell21, myCell22, myCell23, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 13 Opadd */{ myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell25, myCell25, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0 },
+	/* 14 Opmul */{ myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell0, myCell26, myCell27, myCell28, myCell0, myCell0, myCell0, myCell0 },
+};
 
 class Parser {
 
 
 	//This cell 0 will be used to throw the error for the blank spaces in the matrix.
 
-	cell myCell0 = { 0, "", "" };
-
-	cell myCell1 = { 1, "Pgm", "kwdprog brace1 Slist brace2" };
-
-	cell myCell2 = { 2, "Slist", "Stmt semi Slist" };
-
-	cell myCell3 = { 3, "Slist", "eps" };
-
-	cell myCell4{ 4, "Stmt", "S_Out" };
-
-	cell myCell5 = { 5, "Stmt", "id equal Y1" };
-
-	cell myCell6 = { 6, "S_Out", "kwdprint paren1 Elist paren2" };
-
-	cell myCell7 = { 7, "Y1", "E" };
-
-	cell myCell8 = { 8, "Y1", "kwdinput" };
-
-	cell myCell9 = {9, "Elist", "E Elist2"};
-    
-	cell myCell10 = { 10, "Elist", "eps" };
-    
-	cell myCell11 = { 11, "Elist2", "comma Elist" };
-    
-	cell myCell12 = { 12, "E", "T X1" };
-    
-	cell myCell13 = { 13, "X1", "Opadd T X1" };
-    
-	cell myCell14 = { 14, "X1", "eps" };
-    
-	cell myCell15 = { 15, "T", "F X2" };
-    
-	cell myCell16 = { 16, "X2", "Opmul F X2" };
-    
-	cell myCell117 = { 17, "X2", "eps" };
-    
-	cell mycell18 = { 18, "F", "Fatom" };
-    
-	cell myCell19 = { 19, "F", "paren1 E paren2" };
-    
-	cell myCell20 = { 20, "Fatom", "id" };
-    
-	cell myCell21 = { 21, "Fatom", "int" };
-    
-	cell myCell22 = { 22, "Fatom", "float" };
-    
-	cell myCell23 = { 23, "Fatom", "string" };
-    
-	cell myCell24 = { 24, "Opadd", "plus" };
-    
-	cell myCell25 = { 25, "Opadd", "minus" };
-    
-	cell myCell26 = { 26, "Opmul", "aster" };
-    
-	cell myCell27 = { 27, "Opmul", "slash" };
-    
-	cell myCell28 = { 28, "Opmul", "caret" };
-    
-    cell theMatrix[15][19] =
-    {
-    //              0        1        2        3         4         5         6          7        8         9         10         11        12        13        14        15        16      17        18
-    //              prog     print    input    id        int       float     string     (        )         ,          -         +         *         /         ^         ;         {        }        $
-    /* 0  Pgm   */{ myCell1, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 1  Slist */{ myCell0, myCell2, myCell0, myCell2,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell3, myCell0 },
-    /* 2  Stmt  */{ myCell0, myCell4, myCell0, myCell5,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 3  S_Out */{ myCell0, myCell0, myCell6, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 4  Y1    */{ myCell0, myCell0, myCell8, myCell7,  myCell7,  myCell7,  myCell7,  myCell7,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 5  Elist */{ myCell0, myCell0, myCell0, myCell9,  myCell9,  myCell9,  myCell9,  myCell9,  myCell10, myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 6  Elist2*/{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell11, myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 7  E     */{ myCell0, myCell0, myCell0, myCell12, myCell12, myCell12, myCell12, myCell12, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 8  X1    */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell14, myCell14, myCell13,  myCell13, myCell0,  myCell0,  myCell0, myCell14, myCell0, myCell0, myCell0 },
-    /* 9  T     */{ myCell0, myCell0, myCell0, myCell15, myCell15, myCell15, myCell15, myCell15, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 10 X2    */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell17, myCell17, myCell17,  myCell17, myCell16, myCell16, myCell16, myCell17, myCell0, myCell0, myCell0 },
-    /* 11 F     */{ myCell0, myCell0, myCell0, myCell18, myCell18, myCell18, myCell18, myCell19, myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 12 Fatom */{ myCell0, myCell0, myCell0, myCell20, myCell21, myCell22, myCell23, myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 13 Opadd */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell25, myCell25,  myCell0,  myCell0,  myCell0, myCell0,  myCell0, myCell0, myCell0 },
-    /* 14 Opmul */{ myCell0, myCell0, myCell0, myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,  myCell0,   myCell0,  myCell26, myCell27, myCell28, myCell0, myCell0, myCell0, myCell0 },
-    };
+	
 };
 
 #endif /* Parser_h */
